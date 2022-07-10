ae6e74b981d024e96c77e91094555bb59cf51cc8
Mateus Figueiredo Pereira <figueiredo1497@gmail.com>
2022-07-10 15:46:39 -0300

    feat: Implementation functionality related to products

---
+++ controllers/productsController.js
@@ -0,0 +1,21 @@
+import { ObjectId } from 'mongodb';
+import { db } from '../databases/mongo.js';
+
+export async function getAllProducts(req, res) {
+    try {
+        const products = await db.collection("products").find().toArray();
+        res.send(products).status(302);
+    } catch {
+        res.sendStatus(500);
+    }
+}
+
+export async function getProduct(req, res) {
+    const { _id } = req.body;
+    try {
+        const product = await db.collection("products").find({ _id: ObjectId(_id) }).toArray();
+        res.send(product).status(302);
+    } catch {
+        res.sendStatus(500);
+    }
+}
--- routes/routes.js
+++ routes/routes.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import authRouter from './authRouter.js';
+import productsRouter from './productsRouter.js';
 import testRouter from './test.js';
 
 const router = express.Router();
 router.use(testRouter);
 router.use(authRouter);
+router.use(productsRouter);
 export default router;
